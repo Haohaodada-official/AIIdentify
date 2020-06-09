c62f1c6f0777496d84ec31fb07bd0db5e8f638f9
zzyy12 <915719282@qq.com>
2020-06-09 15:51:21 +0800

    up all

--- main.ts
+++ main.ts
@@ -4,10 +4,50 @@
 
 namespace AIIdentify {
 	
+	export enum ailight {
+        //% block=red
+        Red = 3,
+        //% block=green
+        green = 4,
+        //% block=blue
+        blue = 5
+	}
+	
+	export enum rgbstate{
+		//% block=lightup
+        state1 = 1,
+		//% block=putout
+        state2 = 0
+	}
+	
+	
+	let ai_address = 36;
+	
 	//% weight=40
-    //% blockId=AIIdentify_setup block="get ID"
+    //% blockId=AIIdentify_getid block="get ID"
 	export function getid():void{
-		pins.i2cReadNumber(36, NumberFormat.Int8LE, false);
+		pins.i2cReadNumber(ai_address, NumberFormat.Int8LE, false);
+	}
+	
+	//% weight=40
+    //% blockId=AIIdentify_setnum block="set number|%index"
+	export function setnum(index: number):void{
+		pins.i2cWriteNumber(ai_address, 1, NumberFormat.Int8LE, true);
+		pins.i2cWriteNumber(ai_address, index, NumberFormat.Int8LE, false);
+	}
+	
+	//% weight=40
+    //% blockId=AIIdentify_setthres block="set threshold|%index"
+	export function setthreshold(index: number):void{
+		pins.i2cWriteNumber(ai_address, 2, NumberFormat.Int8LE, true);
+		pins.i2cWriteNumber(ai_address, index, NumberFormat.Int8LE, false);
+	}
+	
+	//% weight=40
+    //% blockId=AIIdentify_setrgb block="set rgb|%light1 state|%rgbstate"
+	export function setrgb(light1: ailight, state: rgbstate):void{
+		pins.i2cWriteNumber(ai_address, light1, NumberFormat.Int8LE, true);
+		pins.i2cWriteNumber(ai_address, state, NumberFormat.Int8LE, false);
 	}
 
 
